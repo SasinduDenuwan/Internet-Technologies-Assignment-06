2dc4f153a383c0f7740326b89a7b99e5d6449e41
SasinduDenuwan <sasindudenuwan2006wpsk@gmail.com>
2025-03-11 13:38:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function appendValue(value){
+    document.getElementById('result').value += value;
+}
+
+function clearValue(){
+    document.getElementById('result').value = '';
+}
+
+function calculateValue(){
+    try{      
+        let displayValue = eval(document.getElementById('result').value);
+        document.getElementById('result').value = displayValue;
+
+    } catch (error){
+        document.getElementById('result').value = "Error";
+        console.log(error);
+    }
+}
